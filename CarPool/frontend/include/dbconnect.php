775076fcc12ada02d1339fe1833bc2c802fdf755
anupam587 <anupamgupta587@gmail.com>
2013-09-18 12:05:44 -0700

    Final

--- CarPool/frontend/include/dbconnect.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $con = mysql_connect("localhost","root","anupam");
-    if (!$con)
-    {
-     die('Could not connect: ' . mysql_error());
-    }
-    mysql_select_db("carpool", $con);
-    
-?>
--- CarPool/frontend/modules/homepage/Creating a Twitter OAuth Application   Nettuts+_files/ajs.php
+++
@@ -1,5 +0,0 @@
-var OX_6461f96b = '';
-OX_6461f96b += "<"+"!-- BEGIN STANDARD TAG - 160 x 600 - 160x600 - Asia: Run-of-site - DO NOT MODIFY -->\n";
-OX_6461f96b += "<"+"SCRIPT TYPE=\"text/javascript\" SRC=\"http://ad.yieldmanager.com/st?ad_type=ad&ad_size=160x600&section=4031282&pub_url=${PUB_URL}\"><"+"/SCRIPT>\n";
-OX_6461f96b += "<"+"!-- END TAG --><"+"div id=\'beacon_661a6bfeb2\' style=\'position: absolute; left: 0px; top: 0px; visibility: hidden;\'><"+"img src=\'http://openx.jeetyetmedia.com/www/delivery/lg.php?bannerid=8462&amp;campaignid=4171&amp;zoneid=989&amp;OACBLOCK=57600&amp;OASCCAP=20&amp;loc=1&amp;referer=http%3A%2F%2Fedge.jeetyetmedia.com%2Fancbanner_general_160x600_265.htm%3Fcat%3D234%2C312%26clientId%3Db42ebe9c-061c-4f1e-adf4-c381dbc5b7fa%26l%3Dhttp%253A%252F%252Fnet.tutsplus.com&amp;cb=661a6bfeb2\' width=\'0\' height=\'0\' alt=\'\' style=\'width: 0px; height: 0px;\' /><"+"/div>\n";
-document.write(OX_6461f96b);
